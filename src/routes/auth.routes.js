1aaf9cc9ae646619eb9bcdd4e382b214c0fb9519
Blink29 <purukumar2905@gmail.com>
2025-05-02 17:44:25 +0530

    fix: google oauth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -10,7 +10,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: process.env.NODE_ENV === 'production'
+      ? 'https://fj-be-r2-paurush-kumar-iitm.onrender.com/api/auth/google/callback'
+      : 'https://fj-be-r2-paurush-kumar-iitm.onrender.com/api/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
     // This callback will pass the profile to our controller
